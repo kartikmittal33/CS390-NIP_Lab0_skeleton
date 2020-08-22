bf8cfc54bf0aa0adc5e2b1041abe5c0f49268637
Maxwell .J. Jacobson <arcosin@users.noreply.github.com>
2020-08-22 01:26:55 -0400

    Update Lab0.py

--- Lab0/Lab0.py
+++ Lab0/Lab0.py
@@ -10,10 +10,11 @@ import random
 # Setting random seeds to keep everything deterministic.
 random.seed(1618)
 np.random.seed(1618)
-tf.set_random_seed(1618)
+#tf.set_random_seed(1618)   # Uncomment for TF1.
+tf.random.set_seed(1618)
 
 # Disable some troublesome logging.
-tf.logging.set_verbosity(tf.logging.ERROR)
+#tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Information on dataset.
